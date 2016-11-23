be415a216acf9e61d25fa295067fa0aaf5bb5cb8
Vinod T R <vinodtr@gmail.com>
2016-11-22 22:01:42 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var mysql = require('mysql');
+var express = require("express");
+var PORT = process.env.port || 3000;
+var app = express();
+var http = require('http').Server(app);
+app.use(express.static(__dirname + '/public'));
+http.listen(PORT, function() {
+	console.log("Web server started !!");
+})
