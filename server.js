573170767e3a0b83049b47b9e96a815798a7b2f1
Vinod T R <vinodtr@gmail.com>
2016-11-22 22:12:29 -0600

    express changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var express = require("express");
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(__dirname + '/public'));
 app.listen(PORT, function() {
